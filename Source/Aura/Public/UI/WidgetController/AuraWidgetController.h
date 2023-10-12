0a75811042b923f3e97b5f97b4f6a23908e67b80
Erik Herlitz <herlitzerik@gmail.com>
2023-10-12 17:06:25 -0400

    Improve EffectActor by starting to implement GameplayEffects

--- Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AuraWidgetController.h
@@ -4,8 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "AbilitySystemComponent.h"
-#include "AbilitySystemGlobals.h"
-#include "UObject/NoExportTypes.h"
 #include "AuraWidgetController.generated.h"
 
 class UAuraAttributeSet;
