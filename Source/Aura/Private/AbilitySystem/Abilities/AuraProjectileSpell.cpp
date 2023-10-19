ec8c25fa8404ffc840fca9ee3e2a84072fd1292d
Erik Herlitz <herlitzerik@gmail.com>
2023-10-19 19:18:52 -0400

    Adds firebolt gameplay ability and starts target data under mouse

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -3,7 +3,9 @@
 
 #include "AbilitySystem/Abilities/AuraProjectileSpell.h"
 
-#include "Kismet/KismetSystemLibrary.h"
+#include "Actor/AuraProjectile.h"
+#include "Interaction/CombatInterface.h"
+
 
 void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
                                            const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
@@ -11,6 +13,34 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
-	UKismetSystemLibrary::PrintString(this, FString("ActivateAbility (C++)"), true, true, FLinearColor::Yellow, 3);
+	
+	
+	
+}
+
+void UAuraProjectileSpell::SpawnProjectile()
+{
+	if (const bool bIsServer = 	GetAvatarActorFromActorInfo()->HasAuthority(); !bIsServer) return;
+
+	if (ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo()))
+	{
+		const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
 		
+		FTransform SpawnTransform;
+		SpawnTransform.SetLocation(SocketLocation);
+		
+		//TODO: Set the Projectile rotation
+		
+		AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
+			ProjectileClass,
+			SpawnTransform,
+			GetOwningActorFromActorInfo(),
+			Cast<APawn>(GetOwningActorFromActorInfo()),
+			ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+
+		//TODO: Give the Projectile a gameplay effect spec for causing damage
+
+		
+		Projectile->FinishSpawning(SpawnTransform);
+	}
 }
